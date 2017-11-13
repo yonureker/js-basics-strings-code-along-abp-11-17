de1816de60de20208e5ce6283642ac9270cb8a43
Onur Eker <yonureker@gmail.com>
2017-11-13 05:53:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ let currentUser = 'Grace Hopper';
 let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
