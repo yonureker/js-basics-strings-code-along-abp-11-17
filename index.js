26c92b52576cd17b4482b7c41fbdcafc5d1c88d7
Onur Eker <yonureker@gmail.com>
2017-11-13 05:49:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Write your code in this file!
 const currentUser = 'Grace Hopper';
 
-const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
 
 const excitedWelcomeMessage = welcomeMessage.uppercase
