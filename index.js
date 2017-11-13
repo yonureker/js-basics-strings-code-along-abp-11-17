0c7cbed76985940a6d68e630eb0418b0db44d915
Onur Eker <yonureker@gmail.com>
2017-11-13 05:54:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = 'Grace Hopper';
+
+let welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+let excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+let shortGreeting = `Welcome, ${currentUser[0]}!`;
